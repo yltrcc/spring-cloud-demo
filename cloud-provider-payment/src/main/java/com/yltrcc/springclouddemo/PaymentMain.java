d581f73cbc7f5d29212580a899d8bb75e487c28b
yltrcc <ttxxly@gmail.com>
2021-12-16 11:48:57 +0800

    新建main类

---
+++ cloud-provider-payment/src/main/java/com/yltrcc/springclouddemo/PaymentMain.java
@@ -0,0 +1,15 @@
+package com.yltrcc.springclouddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author yltrcc
+ * @date 2021-12-16 11:47
+ **/
+@SpringBootApplication
+public class PaymentMain {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain.class, args);
+    }
+}
